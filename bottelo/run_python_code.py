e678dc1bf25c5088a34bc370c66324c83e6a473d
Akuli <akuviljanen17@gmail.com>
2024-02-01 04:09:21 +0200

    spawn multiple ahead of time

--- bottelo/run_python_code.py
+++ bottelo/run_python_code.py
@@ -1,5 +1,66 @@
+import atexit
+import queue
 import subprocess
+import threading
 from pathlib import Path
+
+
+# Starting a runner is slow. Let's start many of them ahead of time, and
+# output them as needed.
+class RunnerSpawner:
+    def __init__(self) -> None:
+        self.queue: queue.Queue[subprocess.Popen] = queue.Queue(maxsize=2)
+        self.stopping = False
+        self.thread = threading.Thread(target=self.spawn_more, daemon=True)
+        self.thread.start()
+
+    def spawn_more(self) -> None:
+        while not self.stopping:
+            # --allow-read lets pyodide read Python library files.
+            # This is safe, because pyodide has dummy file system anyway. See tests.
+            process = subprocess.Popen(
+                "ulimit -v 100000000; timeout 10 downloaded-deps/deno run --allow-read run_pyodide.js",
+                shell=True,  # ulimit is provided by shell
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True,
+                cwd=Path(__file__).parent.parent,
+            )
+
+            if self.stopping:
+                process.kill()
+                process.wait()
+            else:
+                self.queue.put(process)  # will wait if queue is full (maxsize)
+
+    def stop(self) -> None:
+        if self.stopping:
+            return
+
+        self.stopping = True
+        while True:
+            try:
+                process = self.queue.get(timeout=0.01)
+            except queue.Empty:
+                if self.thread.is_alive():
+                    # Thread is spawning a new process now. It will soon put
+                    # it to the queue and then stop.
+                    continue
+                else:
+                    # We are done. All processes are stopped and no more will
+                    # be created.
+                    break
+
+            process.kill()
+            process.wait()
+
+    def get_a_process(self) -> subprocess.Popen:
+        return self.queue.get()
+
+
+spawner = RunnerSpawner()
+atexit.register(spawner.stop)
 
 
 # Uses pyodide (python interpreter for WebAssembly) as a sandbox.
@@ -8,16 +69,11 @@ from pathlib import Path
 # To run webassembly, I stole some javascript code from pyodide's tests.
 # I am running it with deno because they run it with deno.
 def run_python_code(code: str) -> str:
-    output = subprocess.check_output(
-        [
-            "downloaded-deps/deno",
-            "run",
-            "--allow-read",  # let it read library files (pyodide has dummy file system)
-            "run_pyodide.js",
-            code,
-        ],
-        #stderr=subprocess.STDOUT,
-        text=True,
-        cwd=Path(__file__).parent.parent,
-    )
-    return output.replace('\n', ' ').strip()
+    process = spawner.get_a_process()
+    try:
+        process.stdin.write(code)
+        process.stdin.flush()
+        process.stdin.close()
+        return process.stdout.read().replace('\n', ' ').strip()
+    finally:
+        process.wait()
--- run_pyodide.js
+++ run_pyodide.js
@@ -1,13 +1,18 @@
-// taken from pyodide's tests
-// https://github.com/pyodide/pyodide/releases/tag/0.25.0
+// based on pyodide's tests
+// https://github.com/pyodide/pyodide/blob/3e6d17147b60423b729f1b4920032e11185dfc42/src/test-deno/smoke-test.ts
 
 import pyodideModule from "npm:pyodide@0.25.0/pyodide.js";
 const { loadPyodide, toPy } = pyodideModule;
 
-const pyodide = await loadPyodide();
+
+const pyodide = await loadPyodide();  // This is really really slow.
+
+// Read python code after loading pyodide to work around loading slowness.
+// This way, we can prepare a runners when idle, and have it sit here until it's needed.
+const pythonCode = await Deno.readTextFile("/dev/stdin");
 
 try {
-  const result = await pyodide.runPythonAsync(Deno.args[0])
+  const result = await pyodide.runPythonAsync(pythonCode);
   // Show the Python return value / result, if not None
   if (result !== undefined) {
     console.log(result.toString());
