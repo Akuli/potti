7a8ed298494467f5c8bc60796bc848d2cd624e1e
Akuli <akuviljanen17@gmail.com>
2024-02-05 15:30:30 +0200

    Deno permissions: Allow reading only specific things (#6)

--- potti/run_python_code.py
+++ potti/run_python_code.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import fcntl
 import atexit
@@ -48,9 +49,9 @@ class RunnerPool:
 
     def spawn_one_new_runner(self) -> subprocess.Popen[bytes]:
         process = subprocess.Popen(
-            # --allow-read lets pyodide read Python library files.
-            # This is safe, because pyodide has dummy file system anyway. See tests.
-            ["deno/deno", "run", "--allow-read", "run_pyodide.js"],
+            # We need to be careful with permissions, because it's possible to
+            # run arbitrary javascript code. See tests.
+            ["deno/deno", "run", "--allow-read=deno/cache/npm/registry.npmjs.org/pyodide,run_pyodide.js,/dev/stdin", "run_pyodide.js"],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
@@ -185,6 +186,9 @@ def run(code: str) -> str:
             if time.monotonic() > end:
                 return "timed out"
             output += read_available_data(process.stdout)
+
+        # Remove ANSI color codes. They show up in some Deno error messages.
+        output = re.sub(rb'\x1b\[\d+m', b'', output)
 
         return output.decode("utf-8", errors="replace").replace('\n', ' ').strip()
 
--- tests/test_run_python_code.py
+++ tests/test_run_python_code.py
@@ -1,13 +1,23 @@
 from potti.run_python_code import run
 
 
-def test_succeeding_codes():
+def test_basic():
     assert run("'a' + 'b'") == 'ab'  # TODO: "'ab'" would be better
     assert run("1 + 2") == '3'
     assert run("[1] + [2]") == '[1, 2]'
     assert run("print(1 + 2)") == '3'
     assert run("3 + 4j") == '(3+4j)'
     assert run("print('x'); print('y'); 'z'") == 'x y z'
+
+
+def test_stdlib_modules():
+    # Built-in module
+    assert run("import math; math") == "<module 'math' (built-in)>"
+    assert run("from math import cos, pi; cos(pi)") == "-1"
+
+    # Standard-library module
+    assert run("import logging; logging") == "<module 'logging' from '/lib/python311.zip/logging/__init__.py'>"
+    assert run("import logging; logging.getLogger('x').warning('test')") == "test"
 
 
 def test_errors():
@@ -43,6 +53,29 @@ def test_print_ddos_attack():
     output = run("while True: print('a', end='', flush=True)")
     assert min_len <= len(output) <= max_len
     assert output.startswith('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
+
+def test_escape_to_javascript_attack(monkeypatch):
+    monkeypatch.setattr("potti.run_python_code.MAX_RUN_TIME", 10)
+
+    # It is possible to run arbitrary javascript code, but it's useless because
+    # of Deno's permissions: https://docs.deno.com/runtime/manual/basics/permissions
+    def run_js(code):
+        return run(f"import pyodide; pyodide.code.run_js({code!r})")
+
+    assert run_js("Deno.readTextFile('/etc/hostname')") == (
+        'PythonError: pyodide.ffi.JsException: PermissionDenied:'
+        ' Requires read access to "/etc/hostname", run again with the --allow-read flag'
+    )
+    assert run_js("Deno.readDir('/home')").startswith(
+        '[object Object] error: Uncaught (in promise) PermissionDenied: Requires read access to "/home"'
+    )
+    assert run_js("Deno.run({cmd: ['bash']})") == (
+        'warning: Use of deprecated "Deno.run()" API. This API will be removed in Deno 2.'
+        ' Run again with DENO_VERBOSE_WARNINGS=1 to get more details.'
+        '  pyodide.ffi.JsException: PermissionDenied:'
+        ' Requires run access to "bash", run again with the --allow-run flag'
+    )
 
 
 def test_no_network_access(monkeypatch):
